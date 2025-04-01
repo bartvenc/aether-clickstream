90863606b3c778fd6395f13c33144cad177a7d69
bartvenc <52706208+bartvenc@users.noreply.github.com>
2025-04-01 23:18:16 +0200

    Initial commit

---
+++ src/main/java/com/aetherstream/aether_clickstream/AetherClickstreamApplication.java
@@ -0,0 +1,13 @@
+package com.aetherstream.aether_clickstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AetherClickstreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AetherClickstreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aetherstream/aether_clickstream/AetherClickstreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aetherstream.aether_clickstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AetherClickstreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
